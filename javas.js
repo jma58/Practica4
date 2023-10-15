cbd4d6fb4be31cbfcfb57522cac8216b9a5f1acd
jma58 <146134573+jma58@users.noreply.github.com>
2023-10-15 16:16:26 +0200

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,29 @@
+
+function scroll(){
+  var myIndex = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("imagenes");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 5000);    
+  }
+}
+
+function modal(){
+  var modal = document.getElementById('login');
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
+
